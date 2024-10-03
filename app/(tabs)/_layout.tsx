8941f44d7b72c53f50b8fb9bb808e02d11825d8f
yorhadev <rafael.takano.dev@gmail.com>
2024-10-03 10:42:38 -0300

    feat: created tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,78 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Redirect, Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useTheme } from "react-native-paper";
 
 export default function TabsLayout() {
+  const theme = useTheme();
+
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.onSurface,
+        tabBarStyle: {
+          backgroundColor: theme.colors.surface,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name="home"
+              size={24}
+              color={focused ? theme.colors.primary : theme.colors.onSurface}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="bookmark"
+        options={{
+          title: "Bookmark",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name="bookmark"
+              size={24}
+              color={focused ? theme.colors.primary : theme.colors.onSurface}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name="plus-circle"
+              size={24}
+              color={focused ? theme.colors.primary : theme.colors.onSurface}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name="user"
+              size={24}
+              color={focused ? theme.colors.primary : theme.colors.onSurface}
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
